d5c1197d8258111068c484fe971c72dbdd034851
Lseoksee <da864268@naver.com>
2024-12-01 23:25:22 +0900

    cuda 테스트

---
+++ Conda & Cuda/build.sh
@@ -0,0 +1,14 @@
+
+# BASE_VERSION=24.04, 22.04(CUDA 사용 가능)
+# CUDA_VERSION=11.8, 12.1
+
+# CUDA 없이 빌드 
+docker buildx build --build-arg "BASE_VERSION=24.04" --platform linux/amd64,linux/arm64 --push -t da864268/conda:ubuntu-24.04 .
+docker buildx build --build-arg "BASE_VERSION=22.04" --platform linux/amd64,linux/arm64 --push -t da864268/conda:ubuntu-22.04 .
+
+# CUDA 빌드
+docker buildx build --build-arg "BASE_VERSION=22.04" --build-arg "CUDA_VERSION=11.8" --platform linux/amd64 --push -t da864268/conda:ubuntu-22.04-cuda11.8 .
+docker buildx build --build-arg "BASE_VERSION=22.04" --build-arg "CUDA_VERSION=12.1" --platform linux/amd64 --push -t da864268/conda:ubuntu-22.04-cuda12.1 .
+
+docker buildx prune -f --all
+docker rm -f buildx_buildkit_multi-arch-builder0 && docker rmi moby/buildkit:buildx-stable-1
