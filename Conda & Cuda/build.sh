0515741d3ef45e4caea45658d1ff618be1ac4419
Lseoksee <da864268@naver.com>
2024-12-02 02:04:20 +0900

    ubuntu 버전 수정

--- Conda & Cuda/build.sh
+++ Conda & Cuda/build.sh
@@ -1,9 +1,9 @@
 
-# BASE_VERSION=24.04, 22.04(CUDA 사용 가능)
+# BASE_VERSION=latest(24.04), 22.04(CUDA 사용 가능)
 # CUDA_VERSION=11.8, 12.1
 
 # CUDA 없이 빌드 
-docker buildx build --build-arg "BASE_VERSION=24.04" --platform linux/amd64,linux/arm64 --push -t da864268/conda:ubuntu-24.04 .
+docker buildx build --platform linux/amd64,linux/arm64 --push -t da864268/conda:ubuntu-latest .
 docker buildx build --build-arg "BASE_VERSION=22.04" --platform linux/amd64,linux/arm64 --push -t da864268/conda:ubuntu-22.04 .
 
 # 빌드정리 (용량문제 발생가능하므로)
