600c232d10daa1241127acaa75190f27c3ec6b57
Lseoksee <da864268@naver.com>
2024-12-02 01:13:05 +0900

    마지막 문법 수정?

--- Conda & Cuda/build.sh
+++ Conda & Cuda/build.sh
@@ -6,9 +6,20 @@
 docker buildx build --build-arg "BASE_VERSION=24.04" --platform linux/amd64,linux/arm64 --push -t da864268/conda:ubuntu-24.04 .
 docker buildx build --build-arg "BASE_VERSION=22.04" --platform linux/amd64,linux/arm64 --push -t da864268/conda:ubuntu-22.04 .
 
-# CUDA 빌드
+# 빌드정리 (용량문제 발생가능하므로)
+docker buildx prune -f --all
+docker rm -f buildx_buildkit_multi-arch-builder0 && docker rmi moby/buildkit:buildx-stable-1
+
+# CUDA 빌드 (11.8)
 docker buildx build --build-arg "BASE_VERSION=22.04" --build-arg "CUDA_VERSION=11.8" --platform linux/amd64 --push -t da864268/conda:ubuntu-22.04-cuda11.8 .
+
+# 빌드정리 (용량문제 발생가능하므로)
+docker buildx prune -f --all
+docker rm -f buildx_buildkit_multi-arch-builder0 && docker rmi moby/buildkit:buildx-stable-1
+
+# CUDA 빌드 (12.1)
 docker buildx build --build-arg "BASE_VERSION=22.04" --build-arg "CUDA_VERSION=12.1" --platform linux/amd64 --push -t da864268/conda:ubuntu-22.04-cuda12.1 .
 
+# 빌드정리 (용량문제 발생가능하므로)
 docker buildx prune -f --all
 docker rm -f buildx_buildkit_multi-arch-builder0 && docker rmi moby/buildkit:buildx-stable-1
